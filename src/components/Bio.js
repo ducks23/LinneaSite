aecfc61f892b5e5b8b36c22fd8ef1cd46ee716db
Jesse Leoanrd <jesse1517@gmail.com>
2019-10-21 23:25:56 -0700

    fixed the about bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -37,7 +37,12 @@ function Bio(){
 
                 <div classname={classes.p}>
                   <h1 id="about">Why I teach</h1>
-                  <p> I teach yoga because I love it dearly and I delight in sharing this practice of connection and curiosity with others. I believe that mindful movement can be healing and empowering and that building a healthy relationship with our own bodies can help us as we pursue healthy relationships with the people and circumstances around us. For the past 19 years of my life yoga has been a secure and consistent source of support and grounding     especially in times of stress and upheaval. My desire is to create communities that feel warm, supportive and safe in which we can explore our humanness with compassion, humor and love.</p>
+                  <p> I teach yoga because I love it dearly and I delight in sharing this practice of connection and curiosity with others. I believe that mindful movement can be healing and
+                      empowering and that building a healthy relationship with our own bodies can help us as we pursue healthy relationships with the people and circumstances around us.
+                    </p>
+                        <p>
+                      For the past 19 years of my life yoga has been a secure and consistent source of support and grounding especially in times of stress and upheaval.
+                      My desire is to create communities that feel warm, supportive and safe in which we can explore our humanness with compassion, humor and love.</p>
                 </div>
             </Grid>
         </Box>
