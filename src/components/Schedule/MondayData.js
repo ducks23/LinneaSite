54eb08eec339f6a04f42bc452a01192386911d69
Jesse Leoanrd <jesse1517@gmail.com>
2020-01-30 13:35:53 -0800

    updated schedule and directions

--- src/components/Schedule/MondayData.js
+++ src/components/Schedule/MondayData.js
@@ -5,8 +5,19 @@ id: 1,
 location: 'North Portland Yoga 55 NE Farragut St',
 time: '12:00-1:00pm',
 type: 'lunch hour vinyasa',
+directions: 'https://g.page/therealnorthportlandyoga?share',
 pic: 3
+},
+
+{
+id: 1,
+location: 'NE Killingsworth TPY Studio',
+time: '6:00-7:15pm',
+type: 'lunch hour vinyasa',
+directions: 'https://goo.gl/maps/hZG8WNhBuDCV41Ug6',
+pic: 2
 }
+
 
 ]
 
--- src/components/Schedule/WednesdayData.js
+++ src/components/Schedule/WednesdayData.js
@@ -4,12 +4,13 @@ const WednesdayData = [
     location: 'North Portland Yoga 55 NE Farragut St',
     time: '12:00-1:00pm',
     type: 'Lunch Hour Vinyasa',
+    directions: 'https://g.page/therealnorthportlandyoga?share',
     pic: 3
 
   },
   {
     id: 2,
-    location: 'Bhaktishop 2500 SE 26th Ave',
+    location: 'Bhakti Yoga MC 2500 SE 26th Ave',
     time: '4:00-5:00pm',
     type: 'Yoga 2',
     directions: "https://goo.gl/maps/N4EC2NP79vs5ve7T6",
@@ -18,7 +19,7 @@ const WednesdayData = [
   },
   {
     id: 3,
-    location: 'Bhaktishop 2500 SE 26th Ave',
+    location: 'Bhakti Yoga MC 2500 SE 26th Ave',
     time: '5:30-6:45pm',
     type: 'Yoga 1/2',
     directions: "https://goo.gl/maps/N4EC2NP79vs5ve7T6",
